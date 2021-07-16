b59c0167f81f8d46ed81f032f03497c4175a7c99
Bas Zalmstra <zalmstra.bas@gmail.com>
2021-07-16 13:24:38 +0200

    fix: packaging issues

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,22 @@
+// @ts-check
+
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import nodeBuiltins from 'builtin-modules';
 
+/** @type { import('rollup').RollupOptions } */
 export default {
     input: 'dist/main.js',
-    plugins: [resolve()],
+    plugins: [
+        resolve({
+            preferBuiltins: true
+        }),
+        commonjs()
+    ],
     external: [...nodeBuiltins, 'vscode'],
     output: {
         file: './dist/main.js',
         format: 'cjs',
-        exports: 'named',
+        exports: 'default'
     }
-}
+};
